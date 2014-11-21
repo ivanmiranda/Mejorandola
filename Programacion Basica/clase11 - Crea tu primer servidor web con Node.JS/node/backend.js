9f063d94bb6ab0da0e99c22d8e783e23a66a3fd9
Juan Flores <juancfg_18@hotmail.com>
2014-11-20 16:59:17 -0800

    Profesional de FrontEnd, primera clase

--- Programacion Basica/clase11 - Crea tu primer servidor web con Node.JS/node/backend.js
+++ Programacion Basica/clase11 - Crea tu primer servidor web con Node.JS/node/backend.js
@@ -1,14 +1,38 @@
 //Teclear 127.0.0.1 para apuntar a uno mismo, mientras corres tambien node backend.js, donde se encuentra el archivo desde CMD
 console.log("Arrancando Server de Node");
+basededatos = {
+	usuario : "freddier",
+	password : "mejorandola"
+};
 
 var express = require("express");
+var parcero = require("body-parser");
 var web = express();
+web.use(parcero.urlencoded());
 var servidor;
 
 servidor = web.listen(8080, function (){
 	console.log("Servidor Arrancado");
 });
 
+web.get("/", function (requerimiento, respuesta){
+	respuesta.sendfile("formulario.html");
+});
+
+web.post("/entrar", function (requerimiento, respuesta){
+	if(requerimiento.body.usuario == basededatos.usuario && requerimiento.body.clave == basededatos.password)
+	{
+		respuesta.send("Bienvenido al area secreta");
+	}
+	else
+	{
+		respuesta.send("Shu!!, fuera de aqui");
+	}
+	//console.log(requerimiento.body);
+	//respuesta.send("Bienvenido al formulario secreto");
+});
+
+//Prueba de URLs amigables
 //Teclear en navegador 127.0.0.1:8080/test?avion=airbus&edad=28
 web.get("/test", function (requerimiento, respuesta){
 	//Dentro de requerimiento, al irte a console, se ven las variables que se mandaron por GET
