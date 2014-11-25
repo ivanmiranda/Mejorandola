0f76630db9be181c30e012846d5ef498a514030c
Juan Flores <juancfg_18@hotmail.com>
2014-11-25 08:29:36 -0800

    Terminado Curso Prog Basica

--- Programacion Basica/clase12 - Aprende a volar un dron con Javascript/drone/server.js
+++ Programacion Basica/clase12 - Aprende a volar un dron con Javascript/drone/server.js
@@ -1,6 +1,32 @@
 console.log("Iniciando server de Node");
 
+//Drone Control Code
 var arDrone = require("ar-drone");
+var drondinez = arDrone.createClient();
+
+function bateria()
+{
+	console.log("Bateria: " + drondinez.battery());
+}
+
+function despegar_drone()
+{
+	drondinez.config("control:altitude_mac", 100000);
+	drondinez.takeoff();
+	rotar_drone();
+}
+
+function rotar_drone()
+{
+	drondinez.stop();
+	drondinez.calibrate(0);
+}
+
+function aterrizar_drone()
+{
+	drondinez.stop();
+	drondinez.land();
+}
 
 //Express y servidor web
 var express = require("express");
@@ -13,15 +39,20 @@ servidor = web.listen(8080, function(){
 
 web.get("/", function(requerimiento, respuesta){
 	console.log("Home");
+	bateria();
 	respuesta.sendfile("opciones.html");
 });
 
 web.get("/despegar", function(requerimiento, respuesta){
 	console.log("Despegando");
+	bateria();
+	despegar_drone();
 	respuesta.sendfile("opciones.html");
 });
 
 web.get("/aterrizar", function(requerimiento, respuesta){
 	console.log("Aterrizando");
+	aterrizar_drone();
+	bateria();
 	respuesta.sendfile("opciones.html");
 });
