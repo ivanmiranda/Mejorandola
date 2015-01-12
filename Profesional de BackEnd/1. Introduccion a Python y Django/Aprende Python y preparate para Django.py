c1c26418ab941c54aaccb1c549ed6146cc57b691
Juan Flores <juancfg_18@hotmail.com>
2015-01-12 15:57:07 -0800

    Primera clase de python

--- Profesional de BackEnd/1. Introduccion a Python y Django/Aprende Python y preparate para Django.py
+++ Profesional de BackEnd/1. Introduccion a Python y Django/Aprende Python y preparate para Django.py
@@ -84,3 +84,115 @@ print pares
 
 lista1 = [1,2,3,4]
 lista2 = [3,4,5,6]
+lista3 = [1,2,3,7,8,9,1,5,4,3,1]
+
+print set(lista1) - set(lista2)
+print set(lista2) - set(lista1)
+print set(lista2) & set(lista1)
+print set(lista2) | set(lista1)
+print set(lista3)
+
+print lista3
+lista3 = list(set(lista3))
+print lista3
+
+print string
+print string[0:6]
+print string[:6]
+print string[3:]
+
+print lista
+print lista[:4]
+print lista[-4:]
+print lista[::2]
+print string[::3]
+print lista
+print lista[0]
+del lista[0]
+print lista[0]
+print diccionario
+del diccionario['c']
+print diccionario
+hoy = 'Hoy es 2015/12/01'
+import re
+print hoy
+print re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\2-\1', hoy)
+cadena = '{}: The Dark {}'
+print cadena
+print cadena.format('Carlos', 'Cow')
+cadena = '{name}: The Dark {thing}'
+print cadena
+print cadena.format(name='Carlos', thing='Cow')
+print cadena.format(thing='Cow', name='Carlos')
+data = {'name': 'Carlos', 'thing': 'Cow'}
+print cadena.format(**data)
+print '%s: The Dark %s' % ('Carlos', 'Cow')
+
+def func():
+    return 'Funcion'
+
+print func()
+
+def func(name, thing):
+    return '{name}: The Dark {thing}'.format(name=name, thing=thing)
+
+print func('Carlos', 'Cow')
+
+def func(*args, **kwargs):
+    print 'args', args
+    print 'kwargs', kwargs
+
+print func('Carlos', 'Cow')
+print func(name='Carlos', thing='Cow')
+
+def func(*args, **kwargs):
+    print 'args', args
+    print 'kwargs', kwargs
+    if args:
+        return '{}: The Dark {}'.format(*args)
+    elif kwargs:
+        return '{name}: The Dark {thing}'.format(**kwargs)
+    else:
+        return 'Nothing'
+
+print func()
+print func('Carlos', 'Cow')
+print func(name='Carlos', thing='Cow')
+
+class Curso():
+    nombre = 'BackEnd'
+    profesor = 'Harvey'
+
+c = Curso()
+print c.nombre
+print c.profesor
+
+d = Curso()
+print d.nombre
+print d.profesor
+
+class Curso():
+    def __init__(self, nombre, profesor):
+        self.nombre = nombre
+        self.profesor = profesor
+
+c = Curso('FrontEnd', 'Leonidas')
+print c
+print c.nombre
+print c.profesor
+
+f = Curso('Backend', 'Harvey')
+print f
+print f.nombre
+print f.profesor
+
+class Curso():
+    def __init__(self, nombre, profesor):
+        self.nombre = nombre
+        self.profesor = profesor
+
+    def info(self):
+        return 'Esta es la clase 1 de {nombre} dictada por {profesor}'.format(nombre=self.nombre, profesor=self.profesor)
+
+f = Curso('FrontEnd', 'Leonidas')
+print f.info()
