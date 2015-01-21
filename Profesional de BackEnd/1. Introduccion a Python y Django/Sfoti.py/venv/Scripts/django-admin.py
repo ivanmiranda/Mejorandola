d29a636b940abbcc497eaad15d566f5406f67db3
Juan Carlos Flores Gonzalez <juancfg_18@hotmail.com>
2015-01-21 06:28:46 -0800

    Sfotipy instalando Django

---
+++ Profesional de BackEnd/1. Introduccion a Python y Django/Sfoti.py/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\xampp\htdocs\Mejorandola\Profesional de BackEnd\1. Introduccion a Python y Django\Sfoti.py\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
