ddfa5ec40997c37e2e47979d8637571ae15b252e
Juan Carlos Flores Gonzalez <juancfg_18@hotmail.com>
2015-01-30 06:13:26 -0800

    django y administrador

--- Profesional de BackEnd/1. Introduccion a Python y Django/Sfoti.py/sfotipy/tracks/admin.py
+++ Profesional de BackEnd/1. Introduccion a Python y Django/Sfoti.py/sfotipy/tracks/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from .models import Track
 
 class TrackAdmin(admin.ModelAdmin):
-	list_display = ('title', 'artist', 'order', 'track_file', 'album')
+	list_display = ('title', 'artist', 'order', 'track_file', 'album', 'player')
+	list_filter = ('artist', 'album')
+	search_fields = ('title', 'artist__first_name','artist__last_name')
 
 admin.site.register(Track, TrackAdmin)
