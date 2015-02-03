2e5fcf92eee993f07727b36d40d85b6c5873c147
Juan Flores <juancfg_18@hotmail.com>
2015-02-02 16:42:12 -0800

    Cambios para reparar django

--- Profesional de BackEnd/1. Introduccion a Python y Django/Sfoti.py/sfotipy/sfotipy/urls.py
+++ Profesional de BackEnd/1. Introduccion a Python y Django/Sfoti.py/sfotipy/sfotipy/urls.py
@@ -1,11 +1,6 @@
 from django.conf.urls import patterns, include, url
-<<<<<<< HEAD
 
 from django.conf import settings
-
-=======
-from django.conf import settings
->>>>>>> origin/master
 from django.contrib import admin
 admin.autodiscover()
 
@@ -13,12 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sfotipy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-<<<<<<< HEAD
     (r'^grappelli/', include('grappelli.urls')), # grappelli URLS
-=======
-
-    (r'^grappelli/', include('grappelli.urls')), #grappelli URLS
->>>>>>> origin/master
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tracks/(?P<title>[\w\-]+)/', 'tracks.views.track_view', name='track_view'),
     url(r'^signup/', 'userprofiles.views.signup', name='signup'),
