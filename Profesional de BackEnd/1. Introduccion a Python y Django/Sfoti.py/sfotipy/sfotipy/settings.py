abb9786c888ed5de33e327df0514852577e84529
Juan Carlos Flores Gonzalez <juancfg_18@hotmail.com>
2015-02-05 06:09:20 -0800

    Pequenos cambios django

--- Profesional de BackEnd/1. Introduccion a Python y Django/Sfoti.py/sfotipy/sfotipy/settings.py
+++ Profesional de BackEnd/1. Introduccion a Python y Django/Sfoti.py/sfotipy/sfotipy/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'sfotipy.middlewares.PaisMiddleware',
 )
 
 ROOT_URLCONF = 'sfotipy.urls'
